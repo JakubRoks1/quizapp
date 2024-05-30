dbae5502dad09d77bbd59d4795da0e208a9664b7
Jakub Roks <kubaroks@onet.eu>
2024-05-30 18:47:29 +0200

    created controllers

---
+++ src/main/java/com/example/quizapp/controller/QuestionController.java
@@ -0,0 +1,26 @@
+package com.example.quizapp.controller;
+
+import com.example.quizapp.model.Question;
+import com.example.quizapp.model.User;
+import com.example.quizapp.repository.QuestionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/questions")
+public class QuestionController {
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    @PostMapping
+    public Question createQuestion(@RequestBody Question question) {
+        return questionRepository.save(question);
+    }
+
+    @GetMapping
+    public List<Question> getAllQuestions() {
+        return questionRepository.findAll();
+    }
+}
