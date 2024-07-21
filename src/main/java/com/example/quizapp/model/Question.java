3aed8b5c69fc7983a6ce895fa071652f2eb54012
Jakub Roks <kubaroks@onet.eu>
2024-07-21 13:52:55 +0200

    Question model modified

--- src/main/java/com/example/quizapp/model/Question.java
+++ src/main/java/com/example/quizapp/model/Question.java
@@ -14,5 +14,9 @@ public class Question {
     private Long id;
 
     private String questionText;
-    private String answer;
+    private String answer1;
+    private String answer2;
+    private String answer3;
+    private String answer4;
+    private String correctAnswer;
 }
