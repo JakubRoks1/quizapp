7d75fc5949c1cc5f0478e7807ecf2029db78f8f3
Jakub Roks <kubaroks@onet.eu>
2024-07-22 19:59:14 +0200

    Model class Answer.java implemented

--- src/main/java/com/example/quizapp/model/Answer.java
+++ src/main/java/com/example/quizapp/model/Answer.java
@@ -6,28 +6,19 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import lombok.Data;
-
-import java.util.Set;
 
 @Entity
 @Data
-public class Question {
+public class Answer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String questionText;
+    private String answerText;
+    private boolean isCorrect;
 
     @ManyToOne
-    @JoinColumn(name = "quiz_id")
-    private Quiz quiz;
-
-    @ManyToOne
-    @JoinColumn(name = "user_id")
-    private User user;
-
-    @OneToMany(mappedBy = "question")
-    private Set<Answer> answers;
+    @JoinColumn(name = "question_id")
+    private Question question;
 }
