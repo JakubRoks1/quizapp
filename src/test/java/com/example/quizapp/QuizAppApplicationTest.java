915ea21b6c80456651570a5fec2d49818a15be2b
Mateusz Szablak <soulshunter@wp.pl>
2025-01-23 19:32:05 +0100

    lekcja - 23-01-2025

--- src/test/java/com/example/quizapp/QuizAppApplicationTest.java
+++ src/test/java/com/example/quizapp/QuizAppApplicationTest.java
@@ -1,12 +1,10 @@
 package com.example.quizapp;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@Disabled
-class QuizAppApplicationTests {
+class QuizAppApplicationTest {
 
     @Test
     void contextLoads() {
